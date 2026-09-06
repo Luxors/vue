261f0a8528553973a3ac0f27eacb09852ebb11cf
Yaroslav V. Derkach <luxors@mail.com>
2026-09-06 22:28:06 +0200

    button comp

--- _weather/src/App.vue
+++ _weather/src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from '@/components/Button.vue'
+</script>
 
-<template>Hello</template>
+<template>
+  <Button />
+</template>
 
 <style scoped></style>
---
+++ _weather/src/components/Button.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+<template>
+  <button
+    type="button"
+    class="app-button"
+  >
+    Save
+  </button>
+</template>
+
+<style lang="css" scoped>
+.app-button {
+  padding: 0.875rem;
+  font-family: var(--font-base);
+  font-size: 1.25rem;
+  font-weight: 600;
+  color: var(--color-primary);
+  border: none;
+  border-radius: 10px;
+  background: var(--l-gradient);
+  cursor: pointer;
+}
+
+.app-button:hover {
+  background: var(--l-gradient-inverted);
+}
+</style>
